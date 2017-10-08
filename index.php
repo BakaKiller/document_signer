177471cb22d62cbbadb7a2db41d46e963a68490f
BakaKiller <totalamor@hotmail.fr>
2017-10-08 23:34:41 +0200

    Made main page table WIP : Signing system

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php
 require_once('includes/start.inc.php');
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
-    header('Location: login.php');
+if (!is_logged_in()) {
+    redirect('login.php');
 }
 
 include_once('includes/header.inc.php');
 ?>
 <div id="content" class="container-fluid">
-    <div class="row justify-content-md-center">
-        <div class="col-6">
-            <form>
-
-            </form>
+    <div class="row">
+        <div class="col">
+            <table class="table table-stripped table-hover">
+                <thead>
+                <tr>
+                    <th>Document</th>
+                    <th>Date</th>
+                    <th>Version</th>
+                    <th>Signé</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach (doc::get_all() as $doc) {
+                    ?>
+                    <tr>
+                        <td><a href="get_file.php?ref=<?php echo $doc->ref; ?>" target="_blank"><?php echo $doc->name; ?></a></td>
+                        <td><?php echo date('d/m/Y', $doc->datecreation); ?></td>
+                        <td><?php echo $doc->lastverion; ?></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
-</body>
-</html>
+<?php
+include_once('includes/footer.inc.php');
