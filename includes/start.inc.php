c44bb424aeb9d765a98aa5ea67afda4d8409e572
BakaKiller <totalamor@hotmail.fr>
2017-10-08 23:30:54 +0200

    updated global includes

--- includes/start.inc.php
+++ includes/start.inc.php
@@ -1,8 +1,12 @@
 <?php
-session_start();
 require_once(dirname(__FILE__) . '/db-init.inc.php');
 require_once(dirname(__FILE__) . '/user.inc.php');
+require_once(dirname(__FILE__) . '/doc.inc.php');
+require_once(dirname(__FILE__) . '/lib.inc.php');
+session_start();
 
 if (isset($_SESSION['blocked']) && $_SESSION['blocked'] > time()) {
     die('You have been blocked for now ! Maybe try later ?');
 }
+
+$user = get_user();
